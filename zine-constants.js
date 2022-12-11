39e2c59d11c158928bbb728eb4362a596edb1aa1
Avaer Kazmer <a@modules.io>
2022-12-11 15:04:32 -0500

    Add physics stride constant

--- zine-constants.js
+++ zine-constants.js
@@ -4,3 +4,5 @@ export const floorNetWorldSize = 100;
 export const floorNetWorldDepth = 1000;
 export const floorNetResolution = 0.1;
 export const floorNetPixelSize = floorNetWorldSize / floorNetResolution;
+
+export const physicsPixelStride = 8;

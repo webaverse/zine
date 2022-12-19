3dc00d058228054ec23afd0a6017dddb11c5f02d
Avaer Kazmer <a@modules.io>
2022-12-19 15:34:12 -0500

    Update zine-constants.js

--- zine-constants.js
+++ zine-constants.js
@@ -5,4 +5,5 @@ export const floorNetWorldDepth = 1000;
 export const floorNetResolution = 0.1;
 export const floorNetPixelSize = floorNetWorldSize / floorNetResolution;
 
+export const pointcloudStride = 4 + 4 + 4 + 1 + 1 + 1;
 export const physicsPixelStride = 8;

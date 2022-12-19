8540cca12d894a1802235ff3bad37424123ab941
Avaer Kazmer <a@modules.io>
2022-12-18 19:59:50 -0500

    Geometry utils extraction cleanup

--- zine-geometry-utils.js
+++ zine-geometry-utils.js
@@ -1,10 +1,4 @@
 import * as THREE from 'three';
-import {
-  floorNetWorldSize,
-  floorNetWorldDepth,
-  floorNetResolution,
-  floorNetPixelSize,
-} from './zine-constants.js';
 
 //
 
@@ -442,28 +436,4 @@ export const getDoubleSidedGeometry = geometry => {
   }
   geometry2.setIndex(new THREE.BufferAttribute(newIndices, 1));
   return geometry2;
-};
-
-//
-
-export const getFloorNetPhysicsMesh = ({
-  floorNetDepths,
-  floorNetCamera,
-  material,
-}) => {
-  const geometry = depthFloat32ArrayToOrthographicGeometry(
-    floorNetDepths,
-    floorNetPixelSize,
-    floorNetPixelSize,
-    floorNetCamera,
-  );
-  geometry.computeVertexNormals();
-
-  // shift geometry by half a floorNetWorldSize
-  geometry.translate(floorNetWorldSize/2, 0, floorNetWorldSize/2);
-  // ...but also shift the mesh to compensate
-  // this centering is required for the physics to work and render correctly
-  const mesh = new THREE.Mesh(geometry, material);
-  mesh.position.set(-floorNetWorldSize/2, 0, -floorNetWorldSize/2);
-  return mesh;
 };

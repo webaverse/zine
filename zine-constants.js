89fdf0f083f7731038c2a6de79b95636b3fb4314
Avaer Kazmer <a@modules.io>
2022-12-25 03:41:57 -0500

    Add entranceExitEmptyDiameter constant

--- zine-constants.js
+++ zine-constants.js
@@ -13,3 +13,5 @@ export const portalExtrusion = 1; // 1m
 export const entranceExitHeight = 2;
 export const entranceExitWidth = 2;
 export const entranceExitDepth = 20;
+
+export const entranceExitEmptyDiameter = portalExtrusion / 2;

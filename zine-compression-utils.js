dc6d4817b15b833e17cf644e9f819afc242b9ab7
Avaer Kazmer <a@modules.io>
2022-12-26 17:28:51 -0500

    Add byte attribute compression utils

--- zine-compression-utils.js
+++ zine-compression-utils.js
@@ -255,6 +255,131 @@ globalThis.testDepthCompression = testDepthCompression;
 
 //
 
+const compressByteAttribute = async (byteAttribute) => {
+  const numPoints = byteAttribute.length;
+  
+  const [
+    encoderModule,
+    // decoderModule,
+  ] = await Promise.all([
+    DracoEncoderModule(),
+    // DracoDecoderModule(),
+  ]);
+  
+  const encoder = new encoderModule.Encoder();
+  const pointCloudBuilder = new encoderModule.PointCloudBuilder();
+  const dracoPointCloud = new encoderModule.PointCloud();
+
+  encoder.SetEncodingMethod(encoderModule.POINT_CLOUD_SEQUENTIAL_ENCODING);
+  encoder.SetAttributeQuantization(encoderModule.POSITION, 0);
+  encoder.SetAttributeQuantization(encoderModule.GENERIC, 0);
+
+  const positionAttribute = pointCloudBuilder.AddUInt8Attribute(dracoPointCloud, encoderModule.POSITION, numPoints, 1, byteAttribute);
+
+  const encodedData = new encoderModule.DracoInt8Array();
+  // Use default encoding setting.
+  // long EncodePointCloudToDracoBuffer(PointCloud pc, boolean deduplicate_values, DracoInt8Array encoded_data);
+  const encodedLen = encoder.EncodePointCloudToDracoBuffer(dracoPointCloud, false, encodedData);
+  const uint8Array = new Uint8Array(encodedLen);
+  const int8Array = new Int8Array(uint8Array.buffer);
+  // const int8Array = new Int8Array(encodedLen);
+  for (let i = 0; i < encodedLen; i++) {
+    int8Array[i] = encodedData.GetValue(i);
+  }
+  const result = uint8Array;
+  // const result = int8Array;
+
+  encoderModule.destroy(encodedData);
+  encoderModule.destroy(dracoPointCloud);
+  encoderModule.destroy(encoder);
+  encoderModule.destroy(pointCloudBuilder);
+
+  return result;
+};
+export const decompressByteAttribute = async byteArray => {
+  const [
+    // encoderModule,
+    decoderModule,
+  ] = await Promise.all([
+    // DracoEncoderModule(),
+    DracoDecoderModule(),
+  ]);
+
+  // Create the Draco decoder.
+  const buffer = new decoderModule.DecoderBuffer();
+  buffer.Init(byteArray, byteArray.length);
+
+  // Create a buffer to hold the encoded data.
+  const decoder = new decoderModule.Decoder();
+  const geometryType = decoder.GetEncodedGeometryType(buffer);
+
+  // Decode the encoded geometry.
+  let outputGeometry;
+  let status;
+  if (geometryType == decoderModule.TRIANGULAR_MESH) {
+    // outputGeometry = new decoderModule.Mesh();
+    // status = decoder.DecodeBufferToMesh(buffer, outputGeometry);
+    throw new Error('decompress failed because the encoded geometry is not a point cloud');
+  } else {
+    outputGeometry = new decoderModule.PointCloud();
+    status = decoder.DecodeBufferToPointCloud(buffer, outputGeometry);
+  }
+
+  const pc = outputGeometry;
+  // long GetAttributeId([Ref, Const] PointCloud pc, draco_GeometryAttribute_Type type);
+  const positionAttribute = decoder.GetAttribute(pc, 0);
+  // boolean GetAttributeFloatForAllPoints([Ref, Const] PointCloud pc, [Ref, Const] PointAttribute pa, DracoFloat32Array out_values);
+  const positionAttributeData = new decoderModule.DracoUInt8Array();
+  decoder.GetAttributeUInt8ForAllPoints(pc, positionAttribute, positionAttributeData);
+
+  // copy data
+  const uint8Array = new Uint8Array(positionAttributeData.size());
+  for (let i = 0; i < uint8Array.length; i++) {
+    uint8Array[i] = positionAttributeData.GetValue(i);
+  }
+
+  // You must explicitly delete objects created from the DracoDecoderModule
+  // or Decoder.
+  decoderModule.destroy(pc);
+  decoderModule.destroy(positionAttribute);
+  decoderModule.destroy(positionAttributeData);
+  decoderModule.destroy(decoder);
+  decoderModule.destroy(buffer);
+
+  return uint8Array;
+};
+const testByteAttributeCompression = async () => {
+  const testData = Uint8Array.from([
+    1, 1, 1,
+    1, 2, 1,
+    1, 1, 1,
+    2, 2, 2,
+    2, 3, 2,
+    2, 2, 2,
+    3, 3, 3,
+    3, 4, 3,
+    3, 3, 3,
+    4, 4, 4,
+    4, 5, 4,
+    4, 4, 4,
+  ].concat(Array(1024).fill(7)));
+  const uint8Array = await compressByteAttribute(testData);
+  const decodedByteAttribute = await decompressByteAttribute(uint8Array);
+  // check that they are the same
+  for (let i = 0; i < testData.length; i++) {
+    if (testData[i] !== decodedByteAttribute[i]) {
+      throw new Error('compression test failed due to data mismatch');
+    }
+  }
+  console.log(`compression test compression ratio: ${compressionRatioString(uint8Array, testData)}`, {
+    testData,
+    decodedDepth,
+  });
+};
+globalThis.testByteAttributeCompression = testByteAttributeCompression;
+
+//
+
 function mergeChunks(chunks) {
   const result = new Uint8Array(chunks.reduce((a, b) => a + b.byteLength, 0));
   let offset = 0;

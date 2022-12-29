b943554ee41dcaddbb354ffcacc9a56aad55d9b8
Avaer Kazmer <a@modules.io>
2022-12-29 04:28:07 -0500

    Update portal constants

--- zine-constants.js
+++ zine-constants.js
@@ -11,7 +11,7 @@ export const physicsPixelStride = 8; // factor to reduce the physics mesh by
 export const portalExtrusion = 1; // 1m
 
 export const entranceExitHeight = 2;
-export const entranceExitWidth = 2;
-export const entranceExitDepth = 10; // backward depth of an exit
+export const entranceExitWidth = 1.5;
+export const entranceExitDepth = 3; // backward depth of an exit
 
 export const entranceExitEmptyDiameter = portalExtrusion / 2;

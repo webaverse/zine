f400a8f766919f84a04e938d2c1feb87ab31fa2f
Avaer Kazmer <a@modules.io>
2023-01-09 10:16:32 -0500

    Add defaultCameraFov constant

--- zine-constants.js
+++ zine-constants.js
@@ -10,6 +10,8 @@ export const floorNetPixelSize = floorNetWorldSize / floorNetResolution;
 export const pointcloudStride = 4 + 4 + 4;
 export const physicsPixelStride = 8; // factor to reduce the physics mesh by
 
+export const defaultCameraFov = 60;
+
 export const portalExtrusion = 1; // 1m
 
 export const entranceExitHeight = 2;
